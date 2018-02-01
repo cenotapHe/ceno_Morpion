2c9029412a7e75324fdce6976711031c2c6c8831
cenotapHe <36037031+cenotapHe@users.noreply.github.com>
2018-02-01 15:23:23 +0100

    Ajout de commentaire dans le code

--- morpion.py
+++ morpion.py
@@ -1,13 +1,20 @@
+# Importation des classes pour le Morpion
 from morpion_class import *
+
+# Importation de la fonction qui permet de choisir un joueur aléatoirement
 from random import choice
 import os
 
-
+# Initialisation du jeu
 new_game = True
 
+# Initialisation d'une nouvelle partie
 partie_en_cours = game()
 
+# Départ d'une nouvelle partie
 while new_game == True :
+	
+	# Boucle permettant d'enchainer plusieurs parties, et de remettre le board a Zero
 
 	partie_en_cours.restart()
 
